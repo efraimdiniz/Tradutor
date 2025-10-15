4e3b652955bcb437f8875bac353916baea3f2ffb
efraimdiniz <efraimd57@gmail.com>
2025-10-15 10:36:49 -0300

    suportando numeros scanner

--- src/main/java/br/ufma/ecp/App.java
+++ src/main/java/br/ufma/ecp/App.java
@@ -3,18 +3,18 @@ package br.ufma.ecp;
 public class App 
 {
     public static void main(String[] args) throws Exception {
-         /*
-        String input = "4-8+6";
+        
+        String input = "498-88+6";
         Scanner scan = new Scanner (input.getBytes());
         System.out.println(scan.nextToken());
         System.out.println(scan.nextToken());
         System.out.println(scan.nextToken());
         System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());*/
-        
+        System.out.println(scan.nextToken());
+        /*
         String input = "8+5+1";
         Parser p = new Parser (input.getBytes());
-        p.parse(); 
+        p.parse(); */
 
     }
 }
