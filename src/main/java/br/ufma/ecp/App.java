108364f1b68eee6dcfd445b50bf4d8dbe5c33b90
efraimdiniz <efraimd57@gmail.com>
2025-10-15 11:30:30 -0300

    Atualizando o Parser

--- src/main/java/br/ufma/ecp/App.java
+++ src/main/java/br/ufma/ecp/App.java
@@ -3,18 +3,18 @@ package br.ufma.ecp;
 public class App 
 {
     public static void main(String[] args) throws Exception {
-        
+        /*
         String input = "289-85+0+6";
         Scanner scan = new Scanner (input.getBytes());
         System.out.println(scan.nextToken());
         System.out.println(scan.nextToken());
         System.out.println(scan.nextToken());
         System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        /*
-        String input = "8+5+1";
+        System.out.println(scan.nextToken());*/
+        
+        String input = "89+508-7+99";
         Parser p = new Parser (input.getBytes());
-        p.parse(); */
+        p.parse();
 
     }
 }
