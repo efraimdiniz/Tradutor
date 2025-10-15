b3978c840b4ec8d4300aaf31a47ddcbfb696d7a6
efraimdiniz <efraimd57@gmail.com>
2025-10-15 14:52:13 -0300

    acrescentando term

--- src/main/java/br/ufma/ecp/App.java
+++ src/main/java/br/ufma/ecp/App.java
@@ -15,12 +15,16 @@ public class App {
         Parser p = new Parser (input.getBytes());
         p.parse();
 
-    }*/
+    }
     String input = "45  + preco - 876";
     Scanner scan = new Scanner (input.getBytes());
     for (Token tk = scan.nextToken(); tk.type != TokenType.EOF; tk = scan.nextToken()) {
         System.out.println(tk);
-    }
+    }*/
 
+    String input = "45  + preco - 876";
+    Parser p = new Parser (input.getBytes());
+    p.parse();
+    
     }
 }
