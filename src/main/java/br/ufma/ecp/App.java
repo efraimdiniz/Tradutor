f7d7276ded7027e98d9f8809bccc5f86ad0b835f
efraimdiniz <efraimd57@gmail.com>
2025-10-15 14:45:44 -0300

    Atualizando o Scanner e Parser

--- src/main/java/br/ufma/ecp/App.java
+++ src/main/java/br/ufma/ecp/App.java
@@ -1,7 +1,6 @@
 package br.ufma.ecp;
 
-public class App 
-{
+public class App {
     public static void main(String[] args) throws Exception {
         /*
         String input = "289-85+0+6";
@@ -10,11 +9,18 @@ public class App
         System.out.println(scan.nextToken());
         System.out.println(scan.nextToken());
         System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());*/
+        System.out.println(scan.nextToken());
         
-        String input = "45  + 89   -       876";
+        String input = "45  + preco - 876";
         Parser p = new Parser (input.getBytes());
         p.parse();
+
+    }*/
+    String input = "45  + preco - 876";
+    Scanner scan = new Scanner (input.getBytes());
+    for (Token tk = scan.nextToken(); tk.type != TokenType.EOF; tk = scan.nextToken()) {
+        System.out.println(tk);
+    }
 
     }
 }
--- src/main/java/br/ufma/ecp/TokenType.java
+++ src/main/java/br/ufma/ecp/TokenType.java
@@ -6,6 +6,7 @@ public enum TokenType {
 
      // Literals.
      NUMBER,
+     IDENT,
 	
 		EOF
 }
